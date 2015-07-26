e3c8e593e6d4c45cb13049fed7df623f3619edfe
subin <subeen_s2m@hotmail.com>
2015-07-27 01:39:32 +0545

    scrolling tab added

--- app/src/main/java/com/project/maharjan/ims/MainActivity.java
+++ app/src/main/java/com/project/maharjan/ims/MainActivity.java
@@ -1,19 +1,35 @@
 package com.project.maharjan.ims;
 
+import android.annotation.TargetApi;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.project.maharjan.ims.adapter.NavDrawerAdapter;
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MaterialTabListener {
 
     private Toolbar toolbar;
     private NavigationDrawerFragment drawerFragment;
+    private ViewPager mPager;
+    private MyPagerAdapter pagerAdapter;
+    private MaterialTabHost tabHost;
 
 
     @Override
@@ -27,6 +43,37 @@ public class MainActivity extends AppCompatActivity {
         drawerFragment = (NavigationDrawerFragment) getFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout)findViewById(R.id.drawer_layout), toolbar);
 
+        mPager = (ViewPager) findViewById(R.id.view_pager);
+        tabHost = (MaterialTabHost)findViewById(R.id.materialTabHost);
+        pagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(pagerAdapter);
+
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                // when user do a swipe the selected tab change
+                tabHost.setSelectedNavigationItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        // insert all tabs from pagerAdapter data
+        for (int i = 0; i < pagerAdapter.getCount(); i++) {
+            tabHost.addTab(
+                    tabHost.newTab()
+                            .setIcon(pagerAdapter.getIcon(i))
+                            .setTabListener(this)
+            );
+        }
     }
 
     @Override
@@ -49,5 +96,64 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(MaterialTab materialTab) {
+        mPager.setCurrentItem(materialTab.getPosition());
+    }
+
+    @Override
+    public void onTabReselected(MaterialTab materialTab) {
+
+    }
+
+    @Override
+    public void onTabUnselected(MaterialTab materialTab) {
+
+    }
+
+    class MyPagerAdapter extends FragmentPagerAdapter {
+        int[] icons = {R.drawable.ic_action_search, R.drawable.ic_action_ic_phone, R.drawable.ic_action_search};
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+       }
+
+        @Override
+        public Fragment getItem(int position) {
+            return MyFragment.getInstance(position);
+
+        }
+
+        @Override
+        public int getCount() {
+            return icons.length;
+        }
+
+        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+        public Drawable getIcon(int position) {
+            return getResources().getDrawable(icons[position], null);
+        }
+    }
+
+    public static class MyFragment extends Fragment {
+        public static MyFragment getInstance(int position) {
+            MyFragment myFragment = new MyFragment();
+            Bundle args = new Bundle();
+            args.putInt("position", position);
+            myFragment.setArguments(args);
+            return myFragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_sliding_tabs, container, false);
+            TextView tv = (TextView) view.findViewById(R.id.tv_sliding_tabs);
+            Bundle bundle = getArguments();
+            if(bundle != null) {
+                tv.setText("The Page number is: " + bundle.getInt("position"));
+            }
+            return view;
+        }
     }
 }
